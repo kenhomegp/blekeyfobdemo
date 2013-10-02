4e727b17fff9e6a2fcc614dcbe72802931f0ec3b
merry <merry@merrytekiMacBook-Pro.local>
2013-10-02 18:16:46 +0800

    First commit_2013_10_2
    This demo is an iOS application for iPhone4S utilizing Bluetooth Low Energy
    to communicate with the CC2540 keyfob
    
    processors.wiki.ti.com/index.php/Category:IPhone4SBLEDemo

---
+++ blekeyfobdemo/TIBLEViewController.h
@@ -0,0 +1,38 @@
+//
+//  TIBLEViewController.h
+//  TI-BLE-Demo
+//
+//  Created by Ole Andreas Torvmark on 10/29/11.
+//  Copyright (c) 2011 ST alliance AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TIBLECBKeyfob.h"
+
+@interface TIBLEViewController : UIViewController <TIBLECBKeyfobDelegate> { 
+    TIBLECBKeyfob *t; //TI keyfob class (private)
+}
+
+// UI elements actions
+- (IBAction)TIBLEUIScanForPeripheralsButton:(id)sender;
+- (IBAction)TIBLEUISoundBuzzerButton:(id)sender;
+
+
+// UI elements outlets
+@property (weak, nonatomic) IBOutlet UIProgressView *TIBLEUIBatteryBar;
+@property (weak, nonatomic) IBOutlet UILabel *TIBLEUIBatteryBarLabel;
+@property (weak, nonatomic) IBOutlet UIProgressView *TIBLEUIAccelXBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *TIBLEUIAccelYBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *TIBLEUIAccelZBar;
+@property (weak, nonatomic) IBOutlet UISwitch *TIBLEUILeftButton;
+@property (weak, nonatomic) IBOutlet UISwitch *TIBLEUIRightButton;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *TIBLEUISpinner;
+@property (weak, nonatomic) IBOutlet UIButton *TIBLEUIConnBtn;
+@property (weak, nonatomic) IBOutlet UIButton *TIBLEUIBuzzer;
+
+
+//Timer methods
+- (void) batteryIndicatorTimer:(NSTimer *)timer;
+- (void) connectionTimer:(NSTimer *)timer;
+
+@end
